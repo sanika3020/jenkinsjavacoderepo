da67f58d64d06f587baf539d531a8a4f082301fa
sanika3020 <shindesanika293@gmail.com>
2025-04-25 12:04:49 +0530

    Create jenkinsfile.java

---
+++ jenkinsfile.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class PrimeChecker {
+
+    public static boolean isPrime(int number) {
+        if (number <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number to check if it is prime: ");
+        int num = scanner.nextInt();
+        if (isPrime(num)) {
+            System.out.println(num + " is a prime number.");
+        } else {
+            System.out.println(num + " is not a prime number.");
+        }
+        scanner.close();
+    }
+}
